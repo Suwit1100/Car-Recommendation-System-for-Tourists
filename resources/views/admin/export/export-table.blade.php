d3a60f7c8911fa118648cc33293b898d6e5ac79e
Suwit1100 <suwitlichoeiwara@gmail.com>
2024-02-15 13:51:13 +0700

    Update export-table.blade.php

--- resources/views/admin/export/export-table.blade.php
+++ resources/views/admin/export/export-table.blade.php
@@ -257,9 +257,7 @@
         <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
         <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
         {{-- JS-Table --}}
-        {{-- Modal --}}
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-        {{-- Modal --}}
+
 
         <script>
             new DataTable('#example');
