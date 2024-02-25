3c23532ac0b07de8547f4c45ba5e2f1f06b0a091
Suwit1100 <suwitlichoeiwara@gmail.com>
2024-02-25 15:48:54 +0700

    ปผ

--- resources/views/user/home/carlist.blade.php
+++ resources/views/user/home/carlist.blade.php
@@ -50,12 +50,12 @@
 
         .box-car-img {
             width: 100%;
-            height: 170px;
+            height: 150px;
         }
 
         .box-car-img img {
             max-width: 100%;
-            max-height: 100%;
+            min-height: 150px;
             object-fit: cover;
             object-position: center center;
         }
