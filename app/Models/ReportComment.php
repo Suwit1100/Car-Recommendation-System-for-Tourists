0ea0984e8d461788f0f523a78f541e2a2c278df4
Suwit1100 <suwitlichoeiwara@gmail.com>
2024-03-02 11:57:45 +0700

    ฟหกฟห

---
+++ app/Models/ReportComment.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReportComment extends Model
+{
+    use HasFactory;
+    use HasFactory;
+    protected $table = 'table_reportcomment';
+    protected $guarded = [];
+    protected $primaryKey = 'id';
+}
---
+++ app/Models/ReportPost.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReportPost extends Model
+{
+    use HasFactory;
+    use HasFactory;
+    protected $table = 'table_reportpost';
+    protected $guarded = [];
+    protected $primaryKey = 'id';
+}
